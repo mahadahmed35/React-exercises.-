4ea86eb1f4e237afb6c8383b172d727798364c0d
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-07-01 19:54:27 +0200

    Add input handling to Input component

--- React-exercises/src/Input.jsx
+++ React-exercises/src/Input.jsx
@@ -62,3 +62,19 @@ const Input = () => {
 }
 
 export default Input
+import {useState} from 'react'
+
+const Input = () => {
+    const[inputValue, setInputValue]= useState("");
+
+  return (
+    <div>
+        <h1>{inputValue}</h1>
+        <input type="text" value={inputValue}
+        onChange={(e)=> setInputValue(e.target.value)}
+        />
+    </div>
+  )
+}
+
+export default Input
